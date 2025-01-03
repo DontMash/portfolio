2baf23c8cdd3453e7deed3eda2f98504488ffffe
Sören Maschmann <smaschmann@slashwhy.de>
2025-01-03 21:47:39 +0100

    feat: add robots txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,17 @@ import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://soren.codes',
   redirects: {
     '/contact': '/#contact',
   },
-  integrations: [
-    icon({
-      iconDir: 'src/assets/icons',
-    }),
-    tailwind({ applyBaseStyles: false }),
-    sitemap({
-      filter: (page) => !/style/.test(page),
-    })],
+  integrations: [icon({
+    iconDir: 'src/assets/icons',
+  }), tailwind({ applyBaseStyles: false }), sitemap({
+    filter: (page) => !/style/.test(page),
+  }), robotsTxt()],
 });
