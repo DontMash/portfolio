e23225a8bb0c1720d863b768540e3560a2ddce07
Sören Maschmann <smaschmann@slashwhy.de>
2025-06-18 00:16:07 +0200

    feat: add localization

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,6 @@ import vercel from '@astrojs/vercel';
 import keystatic from '@keystatic/astro';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
-// import icon from 'astro-icon';
 
 import { defaultLocale, getLocales } from './src/i18n';
 
