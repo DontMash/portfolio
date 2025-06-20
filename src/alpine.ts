0565b26d8a17bb9e3711487f37022be3e558e69d
Sören Maschmann <smaschmann@slashwhy.de>
2025-06-20 13:05:07 +0200

    feat: add dropdown alpinejs component

--- src/alpine.ts
+++ src/alpine.ts
@@ -3,4 +3,21 @@ import collapse from '@alpinejs/collapse';
 
 export default (Alpine: Alpine) => {
   Alpine.plugin(collapse);
+
+  Alpine.data('collapsible', () => ({
+    state: false,
+    toggle() {
+      this.state = !this.state;
+    },
+    open() {
+      this.state = true;
+    },
+    close(focusElement?: HTMLElement) {
+      this.state = false;
+
+      if (focusElement) {
+        focusElement.focus();
+      }
+    },
+  }));
 };
