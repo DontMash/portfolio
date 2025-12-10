d92d71b68381ee9d6a5ce4981b9bd97c009fc06e
Sören Maschmann <smaschmann@slashwhy.de>
2025-12-11 00:44:01 +0100

    feat(style): add more typography options

--- src/components/content/style/style.variant.ts
+++ src/components/content/style/style.variant.ts
@@ -1,6 +1,6 @@
 import { cva, type VariantProps } from 'class-variance-authority';
 
-export const styleVariant = cva([], {
+export const styleVariant = cva(['not-prose'], {
   variants: {
     color: {
       none: null,
@@ -13,6 +13,9 @@ export const styleVariant = cva([], {
       'heading-2': 'heading-2',
       'heading-3': 'heading-3',
       'heading-4': 'heading-4',
+      'heading-5': 'heading-5',
+      'heading-6': 'heading-6',
+      'paragraph-highlight': 'paragraph-highlight',
     },
   },
   defaultVariants: {
