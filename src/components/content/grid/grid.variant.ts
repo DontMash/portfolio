2bd720ae92053819f5803669f41712914b82eb08
Sören Maschmann <smaschmann@slashwhy.de>
2025-12-12 02:08:11 +0100

    refactor(grid): update border style and spacing

--- src/components/content/grid/grid.variant.ts
+++ src/components/content/grid/grid.variant.ts
@@ -1,24 +1,39 @@
 import { cva, type VariantProps } from 'class-variance-authority';
 
-export const gridVariant = cva(
-  ['grid', 'gap-6', 'py-2', 'auto-rows-fr', '*:m-0'],
-  {
-    variants: {
-      columns: {
-        two: ['grid-cols-1', 'md:grid-cols-2'],
-        three: ['grid-cols-1', 'md:grid-cols-2', 'lg:grid-cols-3'],
-        four: ['grid-cols-1', 'lg:grid-cols-2', 'xl:grid-cols-4'],
-      },
-      border: {
-        true: ['*:p-8', 'sm:*:p-4', '*:border-3'],
-        false: null,
-      },
-      background: {
-        true: ['*:bg-(--color-layer-2)'],
-        false: null,
-      },
+export const gridVariant = cva(['grid', 'my-2', 'auto-rows-fr', '*:m-0'], {
+  variants: {
+    columns: {
+      two: ['grid-cols-1', 'md:grid-cols-2'],
+      three: ['grid-cols-1', 'md:grid-cols-2', 'lg:grid-cols-3'],
+      four: ['grid-cols-1', 'lg:grid-cols-2', 'xl:grid-cols-4'],
     },
-    defaultVariants: { columns: 'three', border: true, background: true },
+    border: {
+      true: [
+        '*:px-8',
+        '*:py-4',
+        '*:relative',
+        'overflow-hidden',
+        'border-3',
+        '*:before:absolute',
+        '*:before:z-1',
+        '*:before:bg-(--color-foreground)',
+        '*:before:w-[3px]',
+        '*:before:h-screen',
+        '*:before:left-[100%]',
+        '*:after:absolute',
+        '*:after:z-1',
+        '*:after:bg-(--color-foreground)',
+        '*:after:w-screen',
+        '*:after:h-[3px]',
+        '*:after:top-[100%]',
+      ],
+      false: ['gap-6'],
+    },
+    background: {
+      true: ['bg-(--color-layer-2)'],
+      false: null,
+    },
   },
-);
+  defaultVariants: { columns: 'three', border: true, background: true },
+});
 export type GridProps = VariantProps<typeof gridVariant>;
